40d71aac03680bc5718e8538bd8a5c89ce2afad5
vijayavageesh17 <90508431+vijayavageesh17@users.noreply.github.com>
2022-06-01 19:35:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+mouth_X=0;
+mouth_Y=0;
+
+function preload(){
+    moustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose",gotPoses);
+
+}
+
+function modelLoaded(){
+console.log("PoseNet is initiailized");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+       console.log(results);
+       mouth_X=results[0].pose.nose.x-15;
+       mouth_Y=results[0].pose.nose.y+10;
+       console.log("Mouth X = " + results[0].pose.nose.x);
+       console.log("Mouth Y = " + results[0].pose.nose.y);
+    }
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    image(moustache,mouth_X,mouth_Y,30,30);
+    }
+
+function take_snapshot(){
+    save('My Filter Image');
+}
